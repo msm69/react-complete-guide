25c52b3af88a35bfbee8a71df501a6113af837ce
Daichi Sugawara <dsugawara.home@gmail.com>
2020-12-27 02:41:56 +0900

    名前を変更するための入力欄を作成

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
-import React, { useState } from 'react';
-import './App.css';
-import Person from './Person/Person'
+import React, { useState } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
-  const [persons, setPersons] = useState(
-    [
-      { name: "みずほ2", age: 29 },
-      { name: "太郎2", age: 26 },
-      { name: "次郎2", age: 24 }
-    ]
-  )
+  const [persons, setPersons] = useState([
+    { name: "みずほ2", age: 29 },
+    { name: "太郎2", age: 26 },
+    { name: "次郎2", age: 24 },
+  ]);
 
   const switchNameHandler = (newName) => {
-    setPersons(
-      [
-        { name: newName, age: 30 },
-        { name: "名前変更２", age: 45 },
-        { name: "名前変更３", age: 50 }
-      ])
-  }
+    setPersons([
+      { name: newName, age: 30 },
+      { name: "名前変更２", age: 45 },
+      { name: "名前変更３", age: 50 },
+    ]);
+  };
+
+  const nameChangeHandler = (event) => {
+    setPersons([
+      { name: "みずほ２", age: 30 },
+      { name: event.target.value, age: 45 },
+      { name: "名前変更３", age: 50 },
+    ]);
+  };
 
   return (
     <div className="App">
       <h1>Hi,I`m a React app.</h1>
-      <button onClick={() => switchNameHandler('新しい名前')}>Switch Name</button>
+      <button onClick={() => switchNameHandler("新しい名前")}>
+        Switch Name
+      </button>
       <Person
         name={persons[0].name}
         age={persons[0].age}
-        click={() => switchNameHandler('新しい名前１')} />
+        click={() => switchNameHandler("新しい名前１")}
+      />
       <Person
         name={persons[1].name}
         age={persons[1].age}
-        click={() => switchNameHandler('新しい名前２')} />
+        click={() => switchNameHandler("新しい名前２")}
+        changed={nameChangeHandler}
+      />
       <Person
         name={persons[2].name}
         age={persons[2].age}
-        click={() => switchNameHandler('新しい名前３')} >趣味はプログラミングです</Person>
+        click={() => switchNameHandler("新しい名前３")}
+      >
+        趣味はプログラミングです
+      </Person>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function person(props) {
   return (
     <div>
-      <p onClick={props.click}>私は{props.name}、そして {props.age}歳だよ</p>
+      <p onClick={props.click}>
+        私は{props.name}、そして {props.age}歳だよ
+      </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 }
