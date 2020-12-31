51bdf790259952a77aaddf345f15e5ef320df761
Daichi Sugawara <dsugawara.home@gmail.com>
2020-12-31 20:37:34 +0900

    styled-componentsを利用するよう修正

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,21 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 import "./Person.css";
 
 function person(props) {
-  const style = {
-    "@media (min-width: 500px)": {
-      width: "450px",
-    },
-  };
+  const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 500px) {
+      width: 450px;
+    }
+  `;
 
   return (
-    <div className="Person" style={style}>
+    <StyledDiv>
       <p onClick={props.click}>
         私は{props.name}、そして {props.age}歳だよ
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+    </StyledDiv>
   );
 }
 
